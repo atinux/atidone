23138ab9d99b62ba4543221b691c2e1bbb93e878
Sébastien Chopin <seb@nuxt.com>
2023-05-04 09:57:30 +0200

    Update index.post.ts

--- server/api/todos/index.post.ts
+++ server/api/todos/index.post.ts
@@ -1,12 +1,15 @@
 import { useValidatedBody, z } from 'h3-zod'
 
 export default eventHandler(async (event) => {
+  console.log('validate body')
   const { title } = await useValidatedBody(event, {
     title: z.string().min(1).max(100)
   })
+  console.log('validate session')
   const session = await requireUserSession(event)
 
   // List todos for the current user
+  console.log('INsert todo')
   const info = useDb().prepare('INSERT INTO todos (title, userId) VALUES (?, ?)').run(title, session.user.id)
   if (!info.lastInsertRowid) {
     throw createError({
@@ -14,5 +17,6 @@ export default eventHandler(async (event) => {
       message: 'Could not create todo'
     })
   }
+  console.log('query todo')
   return useDb().prepare('SELECT * FROM todos WHERE id = ? AND userId = ?').get(info.lastInsertRowid, session.user.id)
 })
