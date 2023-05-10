61f4396356892a66d45fe351dd875c29ab921f1c
Benjamin Canac <canacb1@gmail.com>
2023-05-10 16:45:13 +0200

    chore: improve design with `@nuxthq/ui` (#1)

--- app.vue
+++ app.vue
@@ -8,41 +8,24 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="container mx-auto min-h-screen flex items-center justify-center">
-    <main>
-      <div class="p-8 border rounded bg-gray-50 shadow-sm">
-        <h1 class="text-3xl pb-4">
-          Todo List
-        </h1>
-        <TodoList v-if="loggedIn" />
-        <div v-else>
-          <p class="text-sm text-gray-500 max-w-sm mb-4">
-            Welcome to Nuxt Todos Edge. A demo hosted on CloudFlare Pages with server-side rendering on the edge and using the <NuxtLink
-              href="https://developers.cloudflare.com/d1/"
-              target="_blank"
-            >
-              D1 database
-            </NuxtLink>.
-          </p>
-          <a
-            href="/api/auth/github"
-            class="inline-block bg-black text-white p-3 py-1 rounded hover:bg-gray-800"
-          >Login with GitHub</a>
-          <p class="text-sm text-gray-500 italic max-w-sm mt-4">
-            No personal informations regarding your GitHub account are stored in database.<br>
-            We store only the todos created linked with your GitHub ID.
-          </p>
-        </div>
-      </div>
-      <footer class="text-center mt-2">
-        <NuxtLink
-          href="https://github.com/atinux/nuxt-todos-edge"
-          target="_blank"
-          class="text-sm text-gray-500 hover:text-gray-700"
-        >
-          Source code on GitHub
-        </NuxtLink>
-      </footer>
-    </main>
-  </div>
+  <UContainer class="min-h-screen flex flex-col justify-center">
+    <TodoList v-if="loggedIn" />
+    <LoginForm v-else />
+
+    <footer class="text-center mt-2">
+      <NuxtLink
+        href="https://github.com/atinux/nuxt-todos-edge"
+        target="_blank"
+        class="text-sm text-gray-500 hover:text-gray-700"
+      >
+        Source code on GitHub
+      </NuxtLink>
+    </footer>
+  </UContainer>
 </template>
+
+<style lang="postcss">
+body {
+  @apply antialiased font-sans text-gray-500 bg-gray-50;
+}
+</style>
