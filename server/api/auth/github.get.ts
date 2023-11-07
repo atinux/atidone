feb8d62fbb876d6a52c3e3792f94f256d8a83d74
Sébastien Chopin <seb@nuxt.com>
2023-11-07 17:55:39 +0100

    chore: move to nuxt-auth-utils (#10)

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -1,43 +1,6 @@
-export default eventHandler(async (event) => {
-  const config = useRuntimeConfig(event)
-  const { code } = getQuery(event)
-
-  if (!code) {
-    // Redirect to GitHub Oauth page
-    let redirectUrl = getRequestURL(event).href
-    // @ts-ignore
-    if (!process.dev) {
-      redirectUrl = redirectUrl.replace('http://', 'https://')
-    }
-    
-    return sendRedirect(event, `https://github.com/login/oauth/authorize?client_id=${config.github.clientId}&redirect_uri=${redirectUrl}`)
+export default oauth.githubEventHandler({
+  async onSuccess(event, { user }) {
+    await setUserSession(event, { user })
+    return sendRedirect(event, '/todos')
   }
-
-  const response: any = await $fetch(
-    'https://github.com/login/oauth/access_token',
-    {
-      method: 'POST',
-      body: {
-        client_id: config.github.clientId,
-        client_secret: config.github.clientSecret,
-        code
-      }
-    }
-  )
-  if (response.error) {
-    return sendRedirect(event, '/')
-  }
-
-  const ghUser: any = await $fetch('https://api.github.com/user', {
-    headers: {
-      'User-Agent': `Github-OAuth-${config.github.clientId}`,
-      Authorization: `token ${response.access_token}`
-    }
-  })
-
-  await setUserSession(event, {
-    user: ghUser
-  })
-
-  return sendRedirect(event, '/todos')
 })
--- server/api/session/index.get.ts
+++
@@ -1,3 +0,0 @@
-export default eventHandler(async (event) => {
-  return await requireUserSession(event)
-})
