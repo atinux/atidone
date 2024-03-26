cd2640f81f9e34d9325ff692b079bdf28526dd8b
Sébastien Chopin <seb@nuxtlabs.com>
2024-03-26 15:31:44 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit'
 
 export default {
   schema: './server/database/schema.ts',
   out: './server/database/migrations'
-} satisfies Config;
+} satisfies Config
