a9602e6fa59418f40f5a73e0255c9ee51bef8e50
Sébastien Chopin <seb@nuxt.com>
2024-05-17 15:12:32 +0200

    chore: update deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
+  dialect: 'sqlite',
   schema: './server/database/schema.ts',
   out: './server/database/migrations'
 } satisfies Config
