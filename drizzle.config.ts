90ddd0092a76f04d6a4fbaaf034c3b788a27f85f
Sébastien Chopin <seb@nuxt.com>
2024-09-18 18:16:51 +0200

    chore: use defineConfig from drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit'
 
-export default {
+export default defineConfig({
   dialect: 'sqlite',
   schema: './server/database/schema.ts',
   out: './server/database/migrations'
-} satisfies Config
+})
