458809a50eebd7b5c3835e76e8928e53165fea82
Eduardo San Martin Morote <posva13@gmail.com>
2024-10-29 11:39:33 +0100

    refactor: use ui types

--- app/utils/errors.ts
+++ app/utils/errors.ts
@@ -1,10 +1,5 @@
 import { ZodError } from 'zod'
-import { H3Error } from 'h3'
 import type { NuxtError } from '#app'
-
-export function isNuxtError(err: unknown): err is NuxtError {
-  return err instanceof H3Error
-}
 
 export function isNuxtZodError(err: unknown): err is NuxtError<{ data: ZodError }> {
   return (
